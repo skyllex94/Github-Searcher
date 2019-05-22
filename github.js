31183212c8c6637f5e92d761ea8c73ed3f58bff1
skyllex94 <45595147+skyllex94@users.noreply.github.com>
2019-05-22 16:50:10 +0300

    desc

--- github.js
+++ github.js
@@ -6,6 +6,7 @@ class Github {
     this.repos_sort = "created: asc";
   }
   async getUser(user) {
+    // Fetching info for the Github API
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${
       this.client_id
     }&client_secret=${this.client_secret}
